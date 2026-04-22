7b71bcc5900f62e4e584cf2ad69c617996fbd04d
i-not <63227534+i-not@users.noreply.github.com>
2026-04-22 12:30:24 +0200

    patch covariance

---
+++ source/prior_kernels.py
@@ -0,0 +1,12 @@
+def gaussian_covariance_from_coords(...):
+    return
+
+def rectangle_subgrid_points(...):
+    return
+
+def patch_covariance_rectangles(...):
+    return
+
+def patch_covariance_matrix(...):
+    return
+
