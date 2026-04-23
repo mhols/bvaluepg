adedfd40c7e40ee6e373d4ed4b5fa1d62ac4d3b4
c0ra <cristi0929@gmail.com>
2026-04-23 12:38:37 +0200

    Removed debug print statement from iteration loop in SmoothRampMixin

--- source/polyagammadensity.py
+++ source/polyagammadensity.py
@@ -504,7 +504,6 @@ class SmoothRampMixin:
 
         idx = 0
         for it in range(total_iter):
-            print(it)
             z = gsm.sample_z_cond_f(f, self.nobs, self.mix)
             #f = gsm.sample_f_cond_z(z, self.nobs, self.prior_mean, self.Lprior, self.mix)
             f = gsm.sample_f_cond_z_cache(z, fz_cache, self.mix)
